154d24cbcae5af19c25af7c3194e8fef16d0cbad
henry_kit <phrasejunior88@gmail.com>
2021-02-01 15:19:28 +0800

    Files of tourism projects (hotels)

---
+++ hotel_search/app.py
@@ -0,0 +1,73 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.graph_objects as go
+import plotly.express as px
+import pandas as pd
+
+app = dash.Dash(__name__)
+
+hotels_df = pd.read_csv("./hotels.csv")
+tour_att_df = pd.read_csv("./tourist_attractions.csv")
+
+tour_att_names = tour_att_df["PAGETITLE"]
+
+app.layout = html.Div([
+    html.Div(
+        className='header',
+        children=[
+            html.H1("Recommendations of Hotels", className='recommendation')
+        ],
+    ),
+    html.Div(children=[
+        html.Div(
+            dcc.Dropdown(
+                id='tour-att',
+                options=[{'label': t, 'value': t} for t in tour_att_names],
+                value=["Eurasian Heritage Centre: Singapore Attraction"],
+                multi=True
+            ),
+            style={'width': '50%', 'display': 'inline-block'})
+    ]),
+
+    dcc.Graph(id='display-map')
+])
+
+@app.callback(
+    Output('display-map', 'figure'),
+    [Input('tour-att', 'value')]
+)
+def update_figure(attractions):
+
+    filtered_tour_att = tour_att_df[tour_att_df["PAGETITLE"].isin(attractions)]
+
+    fig = go.Figure(go.Scattermapbox(
+        mode = "markers",
+        lon = hotels_df["Longitude"],
+        lat = hotels_df["Latitude"],
+        text = hotels_df["Name"],
+        marker = {'size': 10, 'color': 'cyan'}
+    ))
+
+    fig.add_trace(go.Scattermapbox(
+        fill = "toself",
+        lon = filtered_tour_att["LONGTITUDE"],
+        lat = filtered_tour_att["LATITUDE"],
+        text = filtered_tour_att["PAGETITLE"],
+        marker = {'size': 11, 'color': 'darkgoldenrod'}
+    ))
+
+    fig.update_layout(
+        mapbox = {
+            'style': "stamen-terrain",
+            'center': {'lon': 103.8, 'lat': 1.35},
+            'zoom': 10
+        },
+        showlegend = False
+    )
+
+    return fig
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
